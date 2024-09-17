f75af13e72d6d9fc06f8f72ee276c9bb2a5b5860
MJ Brechner <mjbrechner@gmail.com>
2024-09-17 19:55:28 -0400

    fixed sentence/title case capitalization for quotation marks

--- js/converter.js
+++ js/converter.js
@@ -46,6 +46,16 @@ function sentenceCase() {
     }
     textSentenceCase = textSentenceCase.join("! ");
 
+    // When the whole string begins with a single quote/apostrophe, the following should capitalize the first word.
+    if (textSentenceCase.charAt(0) === `'`) {
+        textSentenceCase = `'` + textSentenceCase.charAt(1).toUpperCase() + textSentenceCase.slice(2);
+    }
+
+    // When the whole string begins with a double quote, the following should capitalize the first word.
+    if (textSentenceCase.charAt(0) === `"`) {
+        textSentenceCase = `"` + textSentenceCase.charAt(1).toUpperCase() + textSentenceCase.slice(2);
+    }
+
     document.getElementById("original-text").value = textSentenceCase;
 }
 
@@ -59,6 +69,29 @@ function titleCase() {
         textTitleCase[i] = textTitleCase[i].charAt(0).toUpperCase() + textTitleCase[i].slice(1);
     }
     textTitleCase = textTitleCase.join(' ');
+
+
+   // Capitalize after double quotes
+    textTitleCase = textTitleCase.split(`"`);
+    for (var i = 0; i < textTitleCase.length; i++) {
+        textTitleCase[i] = textTitleCase[i].charAt(0).toUpperCase() + textTitleCase[i].slice(1);
+    }
+    textTitleCase = textTitleCase.join(`"`);
+
+   // Capitalize after single quotes. Because the basic character for a (non-smart) single quote is also an apostrophe, I don't
+   // want to capitalize it in all circumstances. Thus, the following code only capitalizes a single quote following a space. That
+   // should preserve lowercase around aprostrophes but not at the beginning of words. This should work in most cases, except when
+   // a single quote starts the whole string (to be addressed below).
+    textTitleCase = textTitleCase.split(` '`);
+    for (var i = 0; i < textTitleCase.length; i++) {
+        textTitleCase[i] = textTitleCase[i].charAt(0).toUpperCase() + textTitleCase[i].slice(1);
+    }
+    textTitleCase = textTitleCase.join(` '`);
+
+    // When the whole string begins with a single quote/apostrophe, the following should capitalize the first word.
+    if (textTitleCase.charAt(0) === `'`) {
+        textTitleCase = `'` + textTitleCase.charAt(1).toUpperCase() + textTitleCase.slice(2);
+    }
 
     document.getElementById("original-text").value = textTitleCase;
 }
