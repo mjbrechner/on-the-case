fe1138beb154ceb6634c11153a9d42c42e159529
MJ Brechner <mjbrechner@gmail.com>
2024-10-14 15:41:24 -0400

    styling for buttons

--- js/mode-toggle.js
+++ js/mode-toggle.js
@@ -2,7 +2,7 @@
 
 // Toggle mode when the theme button is pressed.
 function modeToggle() {
-    
+
     if (currentMode === "light") {
         currentMode = "dark";
         document.body.style.backgroundColor = "#064635";
@@ -12,6 +12,8 @@ function modeToggle() {
         document.getElementById("text-copied-message").style.backgroundColor = "#F0BB62";
         document.getElementById("text-copied-message").style.color = "#064635";
         document.getElementById("original-text").style.backgroundColor = "#F4EEA9";
+        document.getElementById("copy-button").style.color = "#F4EEA9";
+        document.getElementById("erase-button").style.color = "#F4EEA9";
         localStorage.setItem("currentMode", "dark");
         document.querySelector("a").style.color = "#F0BB62";
     } else {
@@ -23,6 +25,8 @@ function modeToggle() {
         document.getElementById("text-copied-message").style.backgroundColor = "#064635";
         document.getElementById("text-copied-message").style.color = "oldlace";
         document.getElementById("original-text").style.backgroundColor = "white";
+        document.getElementById("copy-button").style.color = "oldlace";
+        document.getElementById("erase-button").style.color = "oldlace";
         localStorage.setItem("currentMode", "light");
         document.querySelector("a").style.color = "#064635";
     }
